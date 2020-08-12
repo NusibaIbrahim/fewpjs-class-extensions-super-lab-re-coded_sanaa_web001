103dc4b8ca8027d768d9905f3aa8f97bff7a1e8f
Nusiba Ibrahim <ainusiba@gmail.com>
2020-08-12 17:52:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your classes here
+
+class Tree {
+  constructor(species) {
+    this.species = species;
+  }
+  static definition() {
+    return "A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves.";
+  }
+}
+let tree = new Tree("Maple");
+tree.species;
+Tree.definition();
+
+class Deciduous extends Tree {
+  constructor(species, name) {
+    super(species);
+    this.name = name;
+  }
+  static definition() {
+    return `${super.definition()} Deciduous trees shed their leaves annually."`;
+  }
+}
+let oak = new Deciduous("Oak", "Professor");
+oak.species;
+oak.name;
+Deciduous.definition();
